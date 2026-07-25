87b7edf8a42f4ddb40a8153d930597af39fd0d1b
Sergey303 <s-agittariu-s@mail.ru>
2026-07-25 17:20:09 +0300

    Add a reusable API test environment

---
+++ tests/Polar.Factograph.Api.Tests/TestHostEnvironment.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.FileProviders;
+
+namespace Polar.Factograph.Api.Tests;
+
+internal sealed class TestHostEnvironment : IHostEnvironment
+{
+    public string EnvironmentName { get; set; } = Environments.Production;
+
+    public string ApplicationName { get; set; } = "Polar.Factograph.Api.Tests";
+
+    public string ContentRootPath { get; set; } = Directory.GetCurrentDirectory();
+
+    public IFileProvider ContentRootFileProvider { get; set; } = new NullFileProvider();
+}
