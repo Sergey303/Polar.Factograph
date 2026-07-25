b396fb7abc83e29dc562c2ffda61aaf4c4d3fc95
Sergey303 <s-agittariu-s@mail.ru>
2026-07-25 19:34:21 +0300

    Extract Fog resource canonicalization

---
+++ src/Polar.Factograph.Fog/FogResourceCanonicalizer.cs
@@ -0,0 +1,44 @@
+using System.Xml.Linq;
+
+namespace Polar.Factograph.Fog;
+
+internal static class FogResourceCanonicalizer
+{
+    private static readonly XNamespace Rdf = LegacyFogVocabulary.RdfNamespace;
+
+    public static FogSourceRecord Canonicalize(
+        FogSourceDescriptor source,
+        long sourceOrdinal,
+        XElement element,
+        DateTime modifiedAt,
+        string? modifiedAtRaw)
+    {
+        string localName = element.Name.LocalName;
+        string resourceId = FogIdentifier.Require(
+            element.Attribute(Rdf + "about")?.Value,
+            source.FogPath,
+            localName);
+        List<FogProperty> properties = new();
+
+        foreach (XElement child in element.Elements())
+        {
+            FogProperty? property = FogPropertyCanonicalizer.Canonicalize(child);
+            if (property is not null)
+            {
+                properties.Add(property);
+            }
+        }
+
+        return new FogSourceRecord(
+            new FogRecordKey(source.FogPath, sourceOrdinal),
+            source.CassetteId,
+            source.CassetteName,
+            FogRecordKind.Resource,
+            resourceId,
+            LegacyFogVocabulary.Namespace + localName,
+            SubstituteTargetId: null,
+            modifiedAt,
+            modifiedAtRaw,
+            properties);
+    }
+}
