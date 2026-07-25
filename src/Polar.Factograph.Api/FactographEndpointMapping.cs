3858932fe4ce309ce7a183727ef8a4fa6dc7680c
Sergey303 <s-agittariu-s@mail.ru>
2026-07-25 17:18:11 +0300

    Map API endpoint modules centrally

---
+++ src/Polar.Factograph.Api/FactographEndpointMapping.cs
@@ -0,0 +1,19 @@
+using Polar.Factograph.Api.Endpoints;
+using Polar.Factograph.Api.Infrastructure;
+
+namespace Polar.Factograph.Api;
+
+public static class FactographEndpointMapping
+{
+    public static WebApplication MapFactographApi(this WebApplication app)
+    {
+        app.UseMiddleware<ApiExceptionMiddleware>();
+        app.MapSystemEndpoints();
+        app.MapProjectEndpoints();
+        app.MapProjectDiagnosticsEndpoints();
+        app.MapIndexEndpoints();
+        app.MapResourceEndpoints();
+        app.MapSearchEndpoints();
+        return app;
+    }
+}
