022546f38fed658dae3c5c8615b99f68b4b39704
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 16:27:44 +0300

    Add typed API contracts

---
+++ src/Polar.Factograph.Web/src/api/models.ts
@@ -0,0 +1,90 @@
+export interface ApiError {
+  code: string;
+  message: string;
+}
+
+export interface ProjectCassetteOverview {
+  id: string;
+  name: string;
+  allowWrite: boolean;
+  rights: string[];
+}
+
+export interface ProjectOverview {
+  projectId: string;
+  name: string;
+  userId: string;
+  projectRights: string[];
+  cassettes: ProjectCassetteOverview[];
+  defaultWriteCassetteId: string | null;
+}
+
+export interface SearchEvidence {
+  predicate: string;
+  value: string;
+  language: string | null;
+}
+
+export interface ResourceSearchResult {
+  resourceId: string;
+  displayName: string;
+  type: string | null;
+  typeLabel: string | null;
+  score: number;
+  sourceCassetteId: string;
+  matches: SearchEvidence[];
+}
+
+export interface PresentedLiteral {
+  predicate: string;
+  label: string;
+  value: string;
+  displayValue: string;
+  language: string | null;
+  dataType: string | null;
+}
+
+export interface PresentedDirectLink {
+  predicate: string;
+  label: string;
+  targetResourceId: string;
+}
+
+export interface PresentedInverseLink {
+  predicate: string;
+  label: string;
+  sourceResourceId: string;
+  sourceCassetteId: string;
+}
+
+export interface ResourceProvenance {
+  sourceRecordId: string;
+  sourceCassetteId: string;
+  sourceFogPath: string;
+  modifiedAt: string;
+}
+
+export interface ResourcePortrait {
+  resourceId: string;
+  type: string | null;
+  typeLabel: string | null;
+  literals: PresentedLiteral[];
+  directLinks: PresentedDirectLink[];
+  inverseLinks: PresentedInverseLink[];
+  provenance: ResourceProvenance;
+}
+
+export interface DocumentLocation {
+  cassetteId: string;
+  cassetteName: string;
+  documentUri: string;
+  folderName: string;
+  documentNumber: string;
+  originalAvailable: boolean;
+  smallPreviewAvailable: boolean;
+  mediumPreviewAvailable: boolean;
+  normalPreviewAvailable: boolean;
+}
+
+export type SearchMode = "names" | "words";
+export type DocumentVariant = "original" | "small" | "medium" | "normal";
