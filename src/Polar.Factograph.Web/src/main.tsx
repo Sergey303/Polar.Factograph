e7e94bb228ffe96b2af4bcffad40b536e853a13b
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 16:33:26 +0300

    Bootstrap React workspace

---
+++ src/Polar.Factograph.Web/src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app/App";
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/components.css";
+
+const root = document.getElementById("root");
+if (root === null) {
+  throw new Error("Application root element was not found.");
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
