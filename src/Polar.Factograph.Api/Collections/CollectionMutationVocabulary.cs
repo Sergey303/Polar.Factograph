2c8ec053ccfc514152dab0a8b4cd253fba54f966
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 13:06:32 +0300

    Add collection mutation vocabulary

---
+++ src/Polar.Factograph.Api/Collections/CollectionMutationVocabulary.cs
@@ -0,0 +1,11 @@
+namespace Polar.Factograph.Api.Collections;
+
+internal static class CollectionMutationVocabulary
+{
+    public const string MembershipType = "collection-member";
+    public const string InCollection = "in-collection";
+    public const string CollectionItem = "collection-item";
+    public const string FullMembershipType = "http://fogid.net/o/collection-member";
+    public const string FullInCollection = "http://fogid.net/o/in-collection";
+    public const string FullCollectionItem = "http://fogid.net/o/collection-item";
+}
