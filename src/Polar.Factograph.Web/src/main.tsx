bb5e6a2ef44d34f925fea76e4afe0c04d5c71a2b
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 16:35:34 +0300

    Load portrait component styles

--- src/Polar.Factograph.Web/src/main.tsx
+++ src/Polar.Factograph.Web/src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./app/App";
 import "./styles/base.css";
 import "./styles/layout.css";
 import "./styles/components.css";
+import "./styles/portrait.css";
 
 const root = document.getElementById("root");
 if (root === null) {
