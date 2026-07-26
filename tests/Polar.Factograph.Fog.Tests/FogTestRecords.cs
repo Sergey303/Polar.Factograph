2089fa4a6b264c0a53ee5f5ae32371b7e2629508
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 09:50:12 +0300

    Generalize Fog async test collection

--- tests/Polar.Factograph.Fog.Tests/FogTestRecords.cs
+++ tests/Polar.Factograph.Fog.Tests/FogTestRecords.cs
@@ -4,14 +4,16 @@ namespace Polar.Factograph.Fog.Tests;
 
 internal static class FogTestRecords
 {
-    public static async Task<List<FogSourceRecord>> ReadAllAsync(
-        FogSourceDescriptor source)
+    public static Task<List<FogSourceRecord>> ReadAllAsync(
+        FogSourceDescriptor source) =>
+        ReadAllAsync(new FileSystemFogRecordReader().ReadAsync(source));
+
+    public static async Task<List<T>> ReadAllAsync<T>(IAsyncEnumerable<T> source)
     {
-        List<FogSourceRecord> result = new();
-        await foreach (FogSourceRecord record in new FileSystemFogRecordReader()
-                           .ReadAsync(source))
+        List<T> result = new();
+        await foreach (T item in source)
         {
-            result.Add(record);
+            result.Add(item);
         }
 
         return result;
