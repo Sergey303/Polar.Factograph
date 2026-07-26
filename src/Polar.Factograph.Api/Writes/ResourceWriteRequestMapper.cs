770b59808c1e12b15398261a08b29f3bbc7fbe41
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 11:03:21 +0300

    Map API resource writes to Fog requests

---
+++ src/Polar.Factograph.Api/Writes/ResourceWriteRequestMapper.cs
@@ -0,0 +1,28 @@
+using Polar.Factograph.Fog;
+
+namespace Polar.Factograph.Api.Writes;
+
+internal static class ResourceWriteRequestMapper
+{
+    public static FogResourceWriteRequest Map(ResourceWriteRequest request)
+    {
+        ResourceWriteRequestValidator.Validate(request);
+        FogProperty[] properties = request.Properties
+            .Select(MapProperty)
+            .ToArray();
+
+        return new FogResourceWriteRequest(
+            request.TypeId,
+            properties,
+            request.ResourceId);
+    }
+
+    private static FogProperty MapProperty(ResourceWritePropertyRequest property) => new(
+        property.Predicate,
+        string.Equals(property.Kind, "resource", StringComparison.OrdinalIgnoreCase)
+            ? FogPropertyKind.Resource
+            : FogPropertyKind.Literal,
+        property.Value,
+        property.Language,
+        property.DataType);
+}
