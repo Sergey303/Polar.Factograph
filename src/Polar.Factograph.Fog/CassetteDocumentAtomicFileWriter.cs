8c4f4c4f4067ae51de68cc377c5b12a06e9933e0
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 14:01:10 +0300

    Split document stream copying

--- src/Polar.Factograph.Fog/CassetteDocumentAtomicFileWriter.cs
+++ src/Polar.Factograph.Fog/CassetteDocumentAtomicFileWriter.cs
@@ -1,13 +1,7 @@
-using System.Security.Cryptography;
-
 namespace Polar.Factograph.Fog;
-
-internal sealed record CassetteDocumentCopyResult(long Length, string Sha256);
 
 internal static class CassetteDocumentAtomicFileWriter
 {
-    private const int BufferSize = 81_920;
-
     public static async Task<CassetteDocumentCopyResult> WriteAsync(
         Stream source,
         string targetPath,
@@ -28,7 +22,7 @@ internal static class CassetteDocumentAtomicFileWriter
         string temporaryPath = targetPath + "." + Guid.NewGuid().ToString("N") + ".tmp";
         try
         {
-            CassetteDocumentCopyResult result = await CopyAsync(
+            CassetteDocumentCopyResult result = await CassetteDocumentStreamCopier.CopyAsync(
                 source,
                 temporaryPath,
                 maxBytes,
@@ -43,46 +37,5 @@ internal static class CassetteDocumentAtomicFileWriter
                 File.Delete(temporaryPath);
             }
         }
-    }
-
-    private static async Task<CassetteDocumentCopyResult> CopyAsync(
-        Stream source,
-        string temporaryPath,
-        long maxBytes,
-        CancellationToken cancellationToken)
-    {
-        await using FileStream output = new(
-            temporaryPath,
-            FileMode.CreateNew,
-            FileAccess.Write,
-            FileShare.None,
-            BufferSize,
-            FileOptions.Asynchronous | FileOptions.SequentialScan);
-        using IncrementalHash hash = IncrementalHash.CreateHash(HashAlgorithmName.SHA256);
-        byte[] buffer = GC.AllocateUninitializedArray<byte>(BufferSize);
-        long length = 0;
-        int read;
-        while ((read = await source.ReadAsync(buffer, cancellationToken)) > 0)
-        {
-            length += read;
-            if (length > maxBytes)
-            {
-                throw new InvalidDataException($"Document exceeds the {maxBytes}-byte upload limit.");
-            }
-
-            hash.AppendData(buffer, 0, read);
-            await output.WriteAsync(buffer.AsMemory(0, read), cancellationToken);
-        }
-
-        if (length == 0)
-        {
-            throw new InvalidDataException("Document content must not be empty.");
-        }
-
-        await output.FlushAsync(cancellationToken);
-        output.Flush(flushToDisk: true);
-        return new CassetteDocumentCopyResult(
-            length,
-            Convert.ToHexString(hash.GetHashAndReset()).ToLowerInvariant());
     }
 }
