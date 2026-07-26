3848f150f1f695c479eba15acadb2f3912c6bec2
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 13:44:27 +0300

    Add document binary response contract

---
+++ src/Polar.Factograph.Api/Documents/DocumentBinaryWriteResponse.cs
@@ -0,0 +1,12 @@
+namespace Polar.Factograph.Api.Documents;
+
+public sealed record DocumentBinaryWriteResponse(
+    string CassetteId,
+    string CassetteName,
+    string DocumentUri,
+    string FolderName,
+    string DocumentNumber,
+    string FileName,
+    long Length,
+    string Sha256,
+    bool Replaced);
