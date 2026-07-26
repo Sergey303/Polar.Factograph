153e5c0ecb5e8583322b5f9bb82e12f48ce0725d
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 18:22:03 +0300

    Separate resource editor schema loading

---
+++ src/Polar.Factograph.Web/src/components/ResourceEditorPane.tsx
@@ -0,0 +1,40 @@
+import type { ProjectCassetteOverview } from "../api/models";
+import type { ResourceWriteResponse } from "../api/resourceWriteModels";
+import type { ResourceDraft } from "../app/resourceDraftModels";
+import { useOntologySchema } from "../app/useOntologySchema";
+import { ResourceEditor } from "./ResourceEditor";
+import { ResourceEditorLoadState } from "./ResourceEditorLoadState";
+
+interface ResourceEditorPaneProps {
+  mode: "create" | "edit";
+  initialDraft: ResourceDraft;
+  cassettes: ProjectCassetteOverview[];
+  token: string;
+  onCancel: () => void;
+  onSaved: (result: ResourceWriteResponse) => void;
+}
+
+export function ResourceEditorPane(props: ResourceEditorPaneProps) {
+  const schema = useOntologySchema(props.token, true);
+  if (schema.schema === null) {
+    return (
+      <ResourceEditorLoadState
+        loading={schema.loading}
+        error={schema.error}
+        onCancel={props.onCancel}
+      />
+    );
+  }
+
+  return (
+    <ResourceEditor
+      mode={props.mode}
+      initialDraft={props.initialDraft}
+      schema={schema.schema}
+      cassettes={props.cassettes}
+      token={props.token}
+      onCancel={props.onCancel}
+      onSaved={props.onSaved}
+    />
+  );
+}
