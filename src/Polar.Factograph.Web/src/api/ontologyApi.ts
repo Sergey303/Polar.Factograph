0c35840c6158931954037ef4b81a33170a783aec
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 18:09:36 +0300

    Add ontology schema API client

---
+++ src/Polar.Factograph.Web/src/api/ontologyApi.ts
@@ -0,0 +1,15 @@
+import { requestJson } from "./http";
+import type { OntologyWriteSchema } from "./ontologyModels";
+
+export const ontologyApi = {
+  getWriteSchema(
+    token: string,
+    signal?: AbortSignal
+  ): Promise<OntologyWriteSchema> {
+    return requestJson<OntologyWriteSchema>(
+      "/api/ontology/write-schema?lang=ru",
+      token,
+      signal
+    );
+  }
+};
