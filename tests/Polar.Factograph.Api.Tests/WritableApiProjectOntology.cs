9c68f489b9db9ad0fab496417813e53c6f696d7a
Sergey303 <s-agittariu-s@mail.ru>
2026-07-26 12:47:42 +0300

    Add object ranges to writable test ontology

--- tests/Polar.Factograph.Api.Tests/WritableApiProjectOntology.cs
+++ tests/Polar.Factograph.Api.Tests/WritableApiProjectOntology.cs
@@ -5,10 +5,22 @@ internal static class WritableApiProjectOntology
     public const string Xml = """
         <?xml version="1.0" encoding="utf-8"?>
         <Ontology xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-          <Class rdf:about="person" />
+          <Class rdf:about="agent" />
+          <Class rdf:about="person">
+            <SubClassOf rdf:resource="agent" />
+          </Class>
+          <Class rdf:about="organization" />
           <DatatypeProperty rdf:about="name">
             <domain rdf:resource="person" />
           </DatatypeProperty>
+          <ObjectProperty rdf:about="mentor">
+            <domain rdf:resource="person" />
+            <range rdf:resource="agent" />
+          </ObjectProperty>
+          <ObjectProperty rdf:about="employer">
+            <domain rdf:resource="person" />
+            <range rdf:resource="organization" />
+          </ObjectProperty>
         </Ontology>
         """;
 }
