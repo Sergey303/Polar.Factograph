35823a4dc6761b1322bf05bc2229e0ea99f28315
Sergey303 <s-agittariu-s@mail.ru>
2026-07-27 17:32:47 +0300

    Add dedicated search page

---
+++ src/Polar.Factograph.Web/src/components/SearchPage.tsx
@@ -0,0 +1,25 @@
+import type { ReturnTypeOfUseSearch } from "../app/useSearchTypes";
+import { SearchPanel } from "./SearchPanel";
+import { SearchResultList } from "./SearchResultList";
+
+interface SearchPageProps {
+  search: ReturnTypeOfUseSearch;
+}
+
+export function SearchPage({ search }: SearchPageProps) {
+  return (
+    <main className="page-shell search-page-shell">
+      <section className="panel search-page-panel">
+        <SearchPanel
+          query={search.query}
+          loading={search.loading}
+          error={search.error}
+          onQueryChange={search.setQuery}
+          onSearch={search.search}
+          onClear={search.clear}
+        />
+        <SearchResultList results={search.results} />
+      </section>
+    </main>
+  );
+}
