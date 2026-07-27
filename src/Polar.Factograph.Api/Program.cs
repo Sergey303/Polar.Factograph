8b38aafec51ae1334c72765433db3905e0205b0f
Sergey303 <s-agittariu-s@mail.ru>
2026-07-27 13:25:04 +0300

    Handle identity storage failures before API endpoints

--- src/Polar.Factograph.Api/Program.cs
+++ src/Polar.Factograph.Api/Program.cs
@@ -1,8 +1,10 @@
 using Polar.Factograph.Api;
+using Polar.Factograph.Api.Authentication;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFactographApi(builder.Configuration, builder.Environment);
 
 WebApplication app = builder.Build();
+app.UseMiddleware<AuthenticationStorageExceptionMiddleware>();
 app.MapFactographApi();
 app.Run();
