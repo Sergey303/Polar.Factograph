759a16b223e2cb42f3ccc35e11e9184149c8dc5c
Sergey303 <s-agittariu-s@mail.ru>
2026-07-27 15:04:04 +0300

    Remove manual collection id navigation

--- src/Polar.Factograph.Web/src/components/NavigationPanel.tsx
+++ src/Polar.Factograph.Web/src/components/NavigationPanel.tsx
@@ -1,14 +1,10 @@
 import type { ProjectOverview } from "../api/models";
-import { CollectionWorkspace } from "./CollectionWorkspace";
 import { ProjectScope } from "./ProjectScope";
 
 interface NavigationPanelProps {
   project: ProjectOverview | null;
   loading: boolean;
   error: string | null;
-  token: string;
-  selectedResourceId: string | null;
-  onSelect: (resourceId: string) => void;
 }
 
 export function NavigationPanel(props: NavigationPanelProps) {
@@ -18,12 +14,6 @@ export function NavigationPanel(props: NavigationPanelProps) {
         project={props.project}
         loading={props.loading}
         error={props.error}
-      />
-      <CollectionWorkspace
-        project={props.project}
-        token={props.token}
-        selectedResourceId={props.selectedResourceId}
-        onSelect={props.onSelect}
       />
     </aside>
   );
