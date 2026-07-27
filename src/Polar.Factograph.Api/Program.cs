a913b055c1745dd37b2754308b89cee699157b07
Sergey303 <s-agittariu-s@mail.ru>
2026-07-27 13:33:51 +0300

    Keep authentication storage handling in the API middleware

--- src/Polar.Factograph.Api/Program.cs
+++ src/Polar.Factograph.Api/Program.cs
@@ -1,10 +1,8 @@
 using Polar.Factograph.Api;
-using Polar.Factograph.Api.Authentication;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFactographApi(builder.Configuration, builder.Environment);
 
 WebApplication app = builder.Build();
-app.UseMiddleware<AuthenticationStorageExceptionMiddleware>();
 app.MapFactographApi();
 app.Run();
