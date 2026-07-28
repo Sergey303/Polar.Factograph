50482ef213fd6e8ae99bacdd3c135c10a65ab8b0
Sergey303 <s-agittariu-s@mail.ru>
2026-07-28 17:21:24 +0300

    Avoid reusing document numbers held by previews

--- src/Polar.Factograph.Fog/CassetteDocumentSlotAllocator.cs
+++ src/Polar.Factograph.Fog/CassetteDocumentSlotAllocator.cs
@@ -5,15 +5,24 @@ namespace Polar.Factograph.Fog;
 
 internal static class CassetteDocumentSlotAllocator
 {
+    private static readonly string[][] DocumentTrees =
+    [
+        ["originals"],
+        ["documents", "small"],
+        ["documents", "medium"],
+        ["documents", "normal"]
+    ];
+
     public static CassetteDocumentSlot Allocate(
         CassetteDefinition cassette,
         string extension)
     {
         ArgumentNullException.ThrowIfNull(cassette);
         ArgumentException.ThrowIfNullOrWhiteSpace(extension);
-        string originals = Path.Combine(Path.GetFullPath(cassette.Path), "originals");
+        string cassetteRoot = Path.GetFullPath(cassette.Path);
+        string originals = Path.Combine(cassetteRoot, "originals");
         Directory.CreateDirectory(originals);
-        (int folder, int document) = FindMaximum(originals);
+        (int folder, int document) = FindMaximum(cassetteRoot);
         (folder, document) = CassetteDocumentNumber.Next(folder, document);
         string folderName = folder.ToString("D4", CultureInfo.InvariantCulture);
         string documentNumber = document.ToString("D4", CultureInfo.InvariantCulture);
@@ -25,10 +34,26 @@ internal static class CassetteDocumentSlotAllocator
             Path.Combine(directory, documentNumber + extension));
     }
 
-    private static (int Folder, int Document) FindMaximum(string originals)
+    private static (int Folder, int Document) FindMaximum(string cassetteRoot)
     {
         (int Folder, int Document) maximum = (0, 0);
-        foreach (string directory in Directory.EnumerateDirectories(originals))
+        foreach (string[] relativeParts in DocumentTrees)
+        {
+            string root = relativeParts.Aggregate(cassetteRoot, Path.Combine);
+            maximum = Max(maximum, FindMaximumInTree(root));
+        }
+        return maximum;
+    }
+
+    private static (int Folder, int Document) FindMaximumInTree(string root)
+    {
+        (int Folder, int Document) maximum = (0, 0);
+        if (!Directory.Exists(root))
+        {
+            return maximum;
+        }
+
+        foreach (string directory in Directory.EnumerateDirectories(root))
         {
             if (!CassetteDocumentNumber.TryParse(
                     Path.GetFileName(directory),
@@ -39,16 +64,19 @@ internal static class CassetteDocumentSlotAllocator
 
             foreach (string file in Directory.EnumerateFiles(directory))
             {
-                if (TryParseDocumentNumber(file, out int document) &&
-                    (folder, document).CompareTo(maximum) > 0)
+                if (TryParseDocumentNumber(file, out int document))
                 {
-                    maximum = (folder, document);
+                    maximum = Max(maximum, (folder, document));
                 }
             }
         }
 
         return maximum;
     }
+
+    private static (int Folder, int Document) Max(
+        (int Folder, int Document) left,
+        (int Folder, int Document) right) => right.CompareTo(left) > 0 ? right : left;
 
     private static bool TryParseDocumentNumber(string path, out int document)
     {
