fd751aa784044bb9a9804dd5a21c2cac632b5612
Sergey303 <s-agittariu-s@mail.ru>
2026-07-29 19:01:09 +0300

    Compose semantic page content as configurable blocks

--- src/Polar.Factograph.Web/src/components/SemanticResourceSections.tsx
+++ src/Polar.Factograph.Web/src/components/SemanticResourceSections.tsx
@@ -1,19 +1,22 @@
 import type { SemanticResourcePage } from "../api/models";
-import { SemanticLinkSection } from "./SemanticLinkSection";
-import { SemanticPhotoGallery } from "./SemanticPhotoGallery";
+import {
+  linkBlock,
+  photoBlock,
+  SemanticContentBlocks
+} from "./SemanticContentBlocks";
 
 interface SemanticResourceSectionsProps {
   page: SemanticResourcePage;
 }
 
 export function SemanticResourceSections({ page }: SemanticResourceSectionsProps) {
-  return (
-    <div className="semantic-resource-sections">
-      <SemanticPhotoGallery photos={page.photos} />
-      <SemanticLinkSection title="Участники" links={page.participants} />
-      <SemanticLinkSection title="Организации" links={page.organizations} />
-      <SemanticLinkSection title="Коллекции" links={page.collections} />
-      <SemanticLinkSection title="Связанные сущности" links={page.relatedResources} />
-    </div>
-  );
+  const blocks = [
+    photoBlock("photos", "Фотографии", page.photos),
+    linkBlock("participants", "Участники", page.participants),
+    linkBlock("organizations", "Организации", page.organizations),
+    linkBlock("collections", "Коллекции", page.collections),
+    linkBlock("related", "Другие связи", page.relatedResources)
+  ];
+
+  return <SemanticContentBlocks blocks={blocks} />;
 }
