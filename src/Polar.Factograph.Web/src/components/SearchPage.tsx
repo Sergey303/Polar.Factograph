526e85d9cad456570452c015628a18afe735faeb
Sergey303 <s-agittariu-s@mail.ru>
2026-07-30 07:16:59 +0300

    Filter search results by addressable type facet

--- src/Polar.Factograph.Web/src/components/SearchPage.tsx
+++ src/Polar.Factograph.Web/src/components/SearchPage.tsx
@@ -1,13 +1,30 @@
+import { useMemo } from "react";
 import { useSearch } from "../app/useSearch";
 import { SearchPanel } from "./SearchPanel";
 import { SearchResultList } from "./SearchResultList";
+import {
+  filterSearchResultsByType,
+  SearchTypeFacets
+} from "./SearchTypeFacets";
 
 interface SearchPageProps {
   search: ReturnType<typeof useSearch>;
+  selectedTypeId: string | null;
   onSearch: (query: string) => void;
+  onTypeChange: (typeId: string | null) => void;
 }
 
-export function SearchPage({ search, onSearch }: SearchPageProps) {
+export function SearchPage({
+  search,
+  selectedTypeId,
+  onSearch,
+  onTypeChange
+}: SearchPageProps) {
+  const visibleResults = useMemo(
+    () => filterSearchResultsByType(search.results, selectedTypeId),
+    [search.results, selectedTypeId]
+  );
+
   return (
     <main className="page-shell search-page-shell">
       <section className="panel search-page-panel">
@@ -17,10 +34,19 @@ export function SearchPage({ search, onSearch }: SearchPageProps) {
           error={search.error}
           onSearch={onSearch}
         />
+        {search.results.length > 0 && (
+          <SearchTypeFacets
+            results={search.results}
+            selectedTypeId={selectedTypeId}
+            onChange={onTypeChange}
+          />
+        )}
         <SearchResultList
           query={search.query}
           loading={search.loading}
-          results={search.results}
+          results={visibleResults}
+          totalResults={search.results.length}
+          typeFiltered={selectedTypeId !== null}
         />
       </section>
     </main>
