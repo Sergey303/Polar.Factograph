393c93ff3543b38884d90a858fd913081384c026
Sergey303 <s-agittariu-s@mail.ru>
2026-07-30 11:27:26 +0300

    Return safe project overview

--- src/Polar.Factograph.Api/Endpoints/ProjectEndpoints.cs
+++ src/Polar.Factograph.Api/Endpoints/ProjectEndpoints.cs
@@ -1,22 +1,7 @@
 using Polar.Factograph.Api.Infrastructure;
 using Polar.Factograph.Application;
-using Polar.Factograph.Domain;
 
 namespace Polar.Factograph.Api.Endpoints;
-
-public sealed record ProjectCassetteOverview(
-    string Id,
-    string Name,
-    bool AllowWrite,
-    IReadOnlyList<string> Rights);
-
-public sealed record ProjectOverview(
-    string ProjectId,
-    string Name,
-    string UserId,
-    IReadOnlyList<string> ProjectRights,
-    IReadOnlyList<ProjectCassetteOverview> Cassettes,
-    string? DefaultWriteCassetteId);
 
 public static class ProjectEndpoints
 {
@@ -35,25 +20,8 @@ public static class ProjectEndpoints
             httpContext,
             cancellationToken);
         _ = ProjectAuthorization.RequireRead(context.Access);
-
-        ProjectCassetteOverview[] cassettes = context.Project.Cassettes
-            .Where(cassette => context.Access.ReadableCassetteIds.Contains(cassette.Id))
-            .OrderBy(cassette => cassette.Name, StringComparer.OrdinalIgnoreCase)
-            .Select(cassette => new ProjectCassetteOverview(
-                cassette.Id,
-                cassette.Name,
-                cassette.AllowWrite,
-                context.Access.Cassettes[cassette.Id].Rights
-                    .Order(StringComparer.Ordinal)
-                    .ToArray()))
-            .ToArray();
-
-        return Results.Ok(new ProjectOverview(
-            context.Project.ProjectId,
-            context.Project.Name,
-            context.Access.UserId,
-            context.Access.ProjectRights.Order(StringComparer.Ordinal).ToArray(),
-            cassettes,
-            context.Access.DefaultWriteCassetteId));
+        return Results.Ok(ProjectOverviewPresentation.Present(
+            context.Project,
+            context.Access));
     }
 }
