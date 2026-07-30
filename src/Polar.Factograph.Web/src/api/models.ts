3b6f0e5a5d3d60d033b6a2d2ef0544e76a3effb4
Sergey303 <s-agittariu-s@mail.ru>
2026-07-30 21:45:26 +0300

    Expose home resource ids to web client

--- src/Polar.Factograph.Web/src/api/models.ts
+++ src/Polar.Factograph.Web/src/api/models.ts
@@ -16,6 +16,7 @@ export interface ProjectOverview {
   canAdmin: boolean;
   cassettes: ProjectCassetteOverview[];
   defaultWriteCassetteId: string | null;
+  homeResourceIds: string[];
 }
 
 export interface SearchEvidence {
