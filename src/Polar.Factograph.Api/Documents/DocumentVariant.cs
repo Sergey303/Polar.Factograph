96345918d7de2b028b4677321c86a000969169f6
Sergey303 <s-agittariu-s@mail.ru>
2026-07-30 08:26:26 +0300

    Add compatible icon document variant

--- src/Polar.Factograph.Api/Documents/DocumentVariant.cs
+++ src/Polar.Factograph.Api/Documents/DocumentVariant.cs
@@ -5,9 +5,10 @@ namespace Polar.Factograph.Api.Documents;
 public enum DocumentVariant
 {
     Original = 1,
-    Small = 2,
-    Medium = 3,
-    Normal = 4
+    Icon = 2,
+    Small = 3,
+    Medium = 4,
+    Normal = 5
 }
 
 public static class DocumentVariantSelector
@@ -15,11 +16,12 @@ public static class DocumentVariantSelector
     public static DocumentVariant Parse(string value) => value.ToLowerInvariant() switch
     {
         "original" => DocumentVariant.Original,
+        "icon" => DocumentVariant.Icon,
         "small" => DocumentVariant.Small,
         "medium" => DocumentVariant.Medium,
         "normal" => DocumentVariant.Normal,
         _ => throw new ArgumentException(
-            "Document variant must be original, small, medium, or normal.",
+            "Document variant must be original, icon, small, medium, or normal.",
             nameof(value))
     };
 
@@ -28,6 +30,7 @@ public static class DocumentVariantSelector
         DocumentVariant variant) => variant switch
     {
         DocumentVariant.Original => location.OriginalPath,
+        DocumentVariant.Icon => location.IconPreviewPath ?? location.SmallPreviewPath,
         DocumentVariant.Small => location.SmallPreviewPath,
         DocumentVariant.Medium => location.MediumPreviewPath,
         DocumentVariant.Normal => location.NormalPreviewPath,
