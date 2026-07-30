fa44b7f84f677dc598248da1bb860f5f433b94da
Sergey303 <s-agittariu-s@mail.ru>
2026-07-30 09:09:13 +0300

    Load ontology category search styles

--- src/Polar.Factograph.Web/src/main.tsx
+++ src/Polar.Factograph.Web/src/main.tsx
@@ -5,6 +5,7 @@ import { App } from "./app/App";
 import "./styles/base.css";
 import "./styles/layout.css";
 import "./styles/components.css";
+import "./styles/ontology-class-search.css";
 import "./styles/portrait.css";
 import "./styles/semantic-resource.css";
 import "./styles/semantic-sections-menu.css";
