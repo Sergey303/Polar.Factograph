c17d9228926969d6e7da3c6334debc8620d9b623
Sergey303 <s-agittariu-s@mail.ru>
2026-08-01 14:42:33 +0300

    Load document page refinements

--- src/Polar.Factograph.Web/src/main.tsx
+++ src/Polar.Factograph.Web/src/main.tsx
@@ -15,6 +15,7 @@ import "./styles/semantic-sections-menu.css";
 import "./styles/semantic-timeline.css";
 import "./styles/collections.css";
 import "./styles/document-actions.css";
+import "./styles/document-page.css";
 import "./styles/document-intake.css";
 import "./styles/resource-editor.css";
 import "./styles/resource-properties.css";
