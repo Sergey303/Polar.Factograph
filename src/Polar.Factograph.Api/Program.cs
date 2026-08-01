896047ef237108fd7139bad5fb7a2aeaf699f6e4
Sergey303 <s-agittariu-s@mail.ru>
2026-08-01 17:39:36 +0300

    Compress large API responses

--- src/Polar.Factograph.Api/Program.cs
+++ src/Polar.Factograph.Api/Program.cs
@@ -1,8 +1,13 @@
 using Polar.Factograph.Api;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
 builder.Services.AddFactographApi(builder.Configuration, builder.Environment);
 
 WebApplication app = builder.Build();
+app.UseResponseCompression();
 app.MapFactographApi();
 app.Run();
